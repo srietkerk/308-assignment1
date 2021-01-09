6d53c69b1cfe865e871e8f69d58dbdf03858712c
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2021-01-08 20:26:51 -0800

    Created the form component that has two input fields for a name and a job that will set character objects.

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,26 +1,10 @@
 import React, { useState } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-            name: 'Charlie',
-            job: 'Janitor',
-        },
-        {
-            name: 'Mac',
-            job: 'Bouncer',
-        },
-        {
-            name: 'Dee',
-            job: 'Aspiring actress',
-        },
-        {
-            name: 'Dennis',
-            job: 'Bartender',
-        },
-    ]);
+    const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -32,6 +16,7 @@ function MyApp() {
     return (
         <div className="container">
             <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form />
         </div>
     );
 }
